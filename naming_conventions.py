391ac45948638779de8e3c8139b223be83b471ac
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-03-29 14:40:41 +0100

    naming conventions

---
+++ naming_conventions.py
@@ -0,0 +1,11 @@
+# NAMING CONVENTIONS (PEP 8)
+
+# a to z, A to Z, 0 to 9 and _
+
+total_count = 100 # valid
+_hidden_value = 42 # valid, but not recommended
+# Starting with a digit = invalid
+# Python is case sensitive
+# Python prefers snake_case, not camelCase
+
+
