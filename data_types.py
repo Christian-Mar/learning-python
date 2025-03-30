04dadfabd2a1a9cd9650a05ba6fe9c01970fc279
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-03-30 12:19:03 +0200

    operators

--- data_types.py
+++ data_types.py
@@ -12,3 +12,16 @@ ip_addresses = {'100.0.0.1', '80.1.1.2'}
 frozen_user_ids = frozenset([1001, 1002, 2003, 543])
 # Dictionaries: Collections of unordered key-value pairs
 person = {'name': 'Alice', 'age': 38, 'is_employed': True}
+
+# Math operators
+
+# Division: / -> resultaat zal een float zijn, bvb 4.0.
+# Floor division: // (=afgerond naar beneden)
+print(18 % 3, 15 / 3, 16 // 3, 2 ** 3)
+
+
+
+print(10%3)
+# Dans cet exemple, le résultat de l'opération 10 % 3 est 1, car 10 divisé par 3 donne 3 avec un reste de 1.
+
+
