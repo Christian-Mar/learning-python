a532ac4c13b5abd5c09b86719a7d5878f918ff2e
Christian <71664957+Christian-Mar@users.noreply.github.com>
2025-03-30 11:51:48 +0200

    data types

---
+++ data_types.py
@@ -0,0 +1,14 @@
+# Number: int and float
+# Booleans -> True and False always starting with an uppercase
+# None
+# Strings -> '' or ""
+# Lists: Ordered, mutable sequences of objects
+person = ['Dan', 40, 82.5, True]
+# Tuples: Ordered, immutable sequences of objects
+coordinates = (40.71, -74.00)
+# Sets: Mutable collections of unordered, unique objects
+ip_addresses = {'100.0.0.1', '80.1.1.2'}
+# FrozenSets: Immutable collections of unordered objects
+frozen_user_ids = frozenset([1001, 1002, 2003, 543])
+# Dictionaries: Collections of unordered key-value pairs
+person = {'name': 'Alice', 'age': 38, 'is_employed': True}
